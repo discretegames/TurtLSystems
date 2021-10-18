f65129acabe8ae7f4c422dfa039a790c8379d077
discretegames <discretizedgames@gmail.com>
2021-10-18 02:04:03 -0700

    Reworked png/eps funcs fir gif prep.

--- src/TurtLSystems/turtlsystems.py
+++ src/TurtLSystems/turtlsystems.py
@@ -26,9 +26,12 @@ DEFAULT_COLORS = (
 PNG_EXT = '.png'
 GIF_EXT = '.gif'
 EPS_EXT = '.eps'
-EPS_NAME = 'drawing'
+FINAL_NAME = 'final'
+FRAME_NAME = 'frame{}'
+DRAW_DIR_NAME = 'draw{}'
 DPI = 96
 
+_DRAW_NUMBER = 0
 _INITIALIZED = False
 _FINISHED = False
 _GHOSTSCRIPT: Optional[str] = None
@@ -85,6 +88,12 @@ def orient(t: turtle.Turtle, position: Tuple[float, float], heading: float) -> N
         t.pendown()
 
 
+def cur_background_color() -> Tuple[int, int, int]:
+    """Returns current turtle graphics background color. Assumes it's a 0-255 tuple."""
+    r, g, b = turtle.bgcolor()
+    return int(r), int(g), int(b)
+
+
 def guess_ghostscript(ghostscript: Optional[str] = None) -> str:
     """Guess the path to ghostscript if it's not already set. Only guesses well on Windows.
     Should prevent people from needing to add ghostscript to PATH.
@@ -109,7 +118,7 @@ def guess_ghostscript(ghostscript: Optional[str] = None) -> str:
     return 'gswin64c'  # Last ditch guess.
 
 
-def init(  # pylint: disable=too-many-arguments,too-many-locals
+def init(
     window_size: Tuple[Union[int, float], Union[int, float]] = (0.75, 0.75),
     window_title: str = "TurtLSystems",
     background_color: Tuple[int, int, int] = (0, 0, 0),
@@ -139,7 +148,7 @@ def init(  # pylint: disable=too-many-arguments,too-many-locals
     turtle.colormode(255)
     turtle.bgcolor(color_tuple(background_color))
     turtle.bgpic(background_image)
-    global _GHOSTSCRIPT, _INITIALIZED  # pylint: disable=global-statement
+    global _GHOSTSCRIPT, _INITIALIZED
     _GHOSTSCRIPT = guess_ghostscript(ghostscript)
     _INITIALIZED = True
 
@@ -148,7 +157,7 @@ def init(  # pylint: disable=too-many-arguments,too-many-locals
 class State:  # pylint: disable=too-many-instance-attributes,too-few-public-methods
     """L-system state."""
 
-    def __init__(  # pylint: disable=too-many-arguments
+    def __init__(
         self,
         position: Tuple[float, float],
         heading: float, angle: float,
@@ -170,7 +179,7 @@ class State:  # pylint: disable=too-many-instance-attributes,too-few-public-meth
         self.change_fill = modify_fill
 
 
-def run(  # pylint: disable=too-many-locals,too-many-branches,too-many-statements
+def run(  # pylint: disable=too-many-branches,too-many-statements
     *,
     t: turtle.Turtle,
     string: str,
@@ -183,7 +192,8 @@ def run(  # pylint: disable=too-many-locals,too-many-branches,too-many-statement
     length_increment: float,
     length_scalar: float,
     thickness_increment: float,
-    color_increments: Tuple[int, int, int]
+    color_increments: Tuple[int, int, int],
+    gif: Optional[str]
 ) -> None:
     """Run turtle `t` on L-system string `string` with given options."""
     initial_angle, initial_length, initial_thickness = angle, length, thickness
@@ -295,8 +305,10 @@ def run(  # pylint: disable=too-many-locals,too-many-branches,too-many-statement
         elif c == '$':
             break
 
+# todo dry up png options?
 
-def draw(  # pylint: disable=too-many-locals,too-many-arguments
+
+def draw(
     start: str = 'F',
     rules: str = 'F F+F-F-F+F',
     angle: float = 90,
@@ -329,15 +341,20 @@ def draw(  # pylint: disable=too-many-locals,too-many-arguments
     exit_on_click: bool = True,
     skip_init: bool = False,
     png: Optional[str] = None,
-    gif: Optional[str] = None,
     padding: Optional[int] = 10,
-    draws_per_frame: int = 1,
     output_scale: float = 1,
     antialiasing: int = 4,
     transparent: bool = False,
+    gif: Optional[str] = None,
+    draws_per_frame: int = 1,
+    sec_per_frame: float = 0.02,
+    max_frames: int = 100,
+    loops: int = 0,
     tmpdir: Optional[str] = None
 ) -> Optional[Tuple[str, Tuple[float, float], float]]:
     """TODO docstring"""
+    global _DRAW_NUMBER
+    _DRAW_NUMBER += 1
     if _FINISHED:
         print('Did not draw() because finish() was already called.')
         return None
@@ -367,12 +384,10 @@ def draw(  # pylint: disable=too-many-locals,too-many-arguments
 
     with ExitStack() as exit_stack:
         if png or gif:
-            if tmpdir:
-                path = pathlib.Path(tmpdir).resolve()
-                path.mkdir(parents=True, exist_ok=True)
-                tmpdir = str(path)
-            else:
+            if not tmpdir:
                 tmpdir = exit_stack.enter_context(TemporaryDirectory())
+            drawdir = pathlib.Path(tmpdir).resolve() / DRAW_DIR_NAME.format(_DRAW_NUMBER)
+            drawdir.mkdir(parents=True, exist_ok=True)
 
         run(t=t,  # todo pass in gif and draws_per_frame
             string=string,
@@ -385,14 +400,16 @@ def draw(  # pylint: disable=too-many-locals,too-many-arguments
             length_increment=scale*length_increment,
             length_scalar=scale*length_scalar,
             thickness_increment=scale*thickness_increment,
-            color_increments=(red_increment, green_increment, blue_increment))
+            color_increments=(red_increment, green_increment, blue_increment),
+            gif=gif)
 
         if asap:
             turtle.tracer(saved_tracer, saved_delay)
             turtle.update()
 
         if png:
-            save_png(png, padding, output_scale, antialiasing, transparent, cast(str, tmpdir))
+            eps = str((drawdir / FINAL_NAME).with_suffix(EPS_EXT))
+            save_png(png, eps, save_eps(eps), output_scale, antialiasing, cur_background_color(), padding, transparent)
 
     if finished:
         finish(exit_on_click, skip_init)
@@ -400,17 +417,19 @@ def draw(  # pylint: disable=too-many-locals,too-many-arguments
     return string, (t.xcor(), t.ycor()), t.heading()
 
 
-def save_canvas_png(png: str, scale: float, antialiasing: int, tmpdir: str) -> None:
+def save_eps(eps: str) -> Tuple[int, int]:
     """TODO docstring"""
-    global _GHOSTSCRIPT  # pylint: disable=global-statement
-    _GHOSTSCRIPT = guess_ghostscript(_GHOSTSCRIPT)  # Re-get ghostscript just in case init was never called.
-    dpi = round(DPI * scale)
-    eps = str(pathlib.Path(tmpdir) / f'{EPS_NAME}{EPS_EXT}')
     canvas = turtle.getcanvas()
     width = max(canvas.winfo_width(), canvas.canvwidth)  # type: ignore
     height = max(canvas.winfo_height(), canvas.canvheight)  # type: ignore
     canvas.postscript(file=eps, x=-width//2, y=-height//2, width=width, height=height)  # type: ignore
-    width, height = round(scale * width), round(scale * height)
+    return width, height
+
+
+def eps_to_png(eps: str, png: str, size: Tuple[int, int], output_scale: float, antialiasing: int) -> None:
+    """TODO docstring"""
+    global _GHOSTSCRIPT
+    _GHOSTSCRIPT = guess_ghostscript(_GHOSTSCRIPT)  # Re-get ghostscript just in case init was never called.
     result = subprocess.run([_GHOSTSCRIPT,
                              '-q',
                              '-dSAFER',
@@ -418,10 +437,10 @@ def save_canvas_png(png: str, scale: float, antialiasing: int, tmpdir: str) -> N
                              '-dNOPAUSE',
                              '-dEPSCrop',
                              '-sDEVICE=pngalpha',
-                             f'-r{dpi}',
-                             f'-g{width}x{height}',
+                             f'-r{round(DPI * output_scale)}',
+                             f'-g{round(output_scale * size[0])}x{round(output_scale * size[1])}',
                              f'-dGraphicsAlphaBits={antialiasing}',
-                             f'-sOutputFile="{png}"',
+                             f'-sOutputFile={png}',  # todo test if quotes needed at all, try png with space
                              eps],
                             check=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8')
     if result.returncode:
@@ -452,18 +471,15 @@ def pad_image(image: Image.Image, padding: int) -> Image.Image:
     return image.crop((x_min, y_min, x_max + 1, y_max + 1))
 
 
-def save_png(  # pylint: disable=too-many-arguments
-        png: str, padding: Optional[int], scale: float, antialiasing: int, transparent: bool, tmpdir: str) -> None:
+def save_png(png: str, eps: str, size: Tuple[int, int], output_scale: float, antialiasing: int,
+             background_color: Tuple[int, int, int], padding: Optional[int], transparent: bool) -> None:
     """TODO docstring"""
     png = str(pathlib.Path(png).with_suffix(PNG_EXT).resolve())
-    save_canvas_png(png, scale, antialiasing, tmpdir)
+    eps_to_png(eps, png, size, output_scale, antialiasing)
     image = Image.open(png).convert('RGBA')
     if padding is not None:
-        image = pad_image(image, round(scale * padding))
-
-    r, g, b = turtle.bgcolor()
-    background_rgba = (int(r), int(g), int(b)) + ((0,) if transparent else (255,))
-    background = Image.new('RGBA', image.size, background_rgba)
+        image = pad_image(image, round(output_scale * padding))
+    background = Image.new('RGBA', image.size, background_color + ((0,) if transparent else (255,)))
     final = Image.alpha_composite(background, image)
     image.close()
     final.save(png)
@@ -474,17 +490,15 @@ def finish(exit_on_click: bool = True, skip_init: bool = False) -> None:
     """Finish drawing and keep window open. Only needed if all calls to `draw` specified `finished=False`."""
     if not skip_init and not _INITIALIZED:
         init()
-    global _FINISHED  # pylint: disable=global-statement
+    global _FINISHED
     if not _FINISHED:
         _FINISHED = True
         (turtle.exitonclick if exit_on_click else turtle.done)()
 
 
 if __name__ == '__main__':
-    init((600, 600), ghostscript='')
+    init((600, 600))
     draw("F-G-G", "F F-G+F+G-F G ,GG", 120, 16, 1, 5, heading=30, position=(-250, 0), png='tri',
-         padding=None, finished=True, asap=True, scale=1, output_scale=1, transparent=True)
-    # draw(png='test.png', finished=False, color=(255, 0, 0), background_color=(20, 20, 20), transparent=False,
-    #      antialiasing=1, scale=.4, show_turtle=False)
-    # draw(png='test.png', finished=False, color=(255, 9, 0))
-    # finish()
+         padding=10, finished=False, asap=True, scale=.4, output_scale=2, transparent=False, tmpdir='tmp')
+    # draw("F-G-G", "F F-G+F+G-F G ,GG", 120, 15, 1, 5, heading=30, position=(-250, 0), png='tri',
+    #      padding=None, finished=False, scale=1, output_scale=1, transparent=True, tmpdir='tmp')
