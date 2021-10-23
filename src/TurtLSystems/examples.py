20af49b098b603d2f2ef32aa86ec5746e5325013
discretegames <discretizedgames@gmail.com>
2021-10-23 14:03:04 -0700

    Moved setup.py to setup.cfg

--- src/TurtLSystems/examples.py
+++ src/TurtLSystems/examples.py
@@ -1,11 +1,11 @@
 """TurtLSystems examples (https://pypi.org/project/TurtLSystems)."""
-
-
+# from TurtLSystems import draw
+# from TurtLSystems import Exit
+# from TurtLSystems import wait
 # from TurtLSystems import init, draw,
 # draw('+A', 'A B-A-B B A+B+A,', 5, 60, 7, 2, (200, 220, 255), None, (36, 8, 107),
 #      red_increment=2, gif='example.gif', max_frames=250, duration=30)
 # # try:
-
 # draw('X', 'X 0F-[[X]+X]-1F[+FX]-X F FF',
 #      5, angle=10, length=5, thickness=2, heading=90, position=(0, -200),
 #      color=(5, 190, 10),
